45c56bf7bc8fa7dd48a72b80c558cac347cd1c57
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-03-21 21:11:05 -0500

    update utils

---
+++ util.py
@@ -0,0 +1,44 @@
+MY_NAME = 'Ednalyn C. De Dios'
+
+import env
+from sqlalchemy import create_engine
+
+def say_hello():
+    'Returns a hello world message to the caller.'
+    return 'Hello, World!'
+
+def get_url_string(user, password, host, db, driver='pymysql'):
+    return f'mysql+{driver}://{user}:{password}@{host}/{db}'
+
+def get_connection(db):
+    url = get_url_string(user=env.user, password=env.password, host=env.host, db=db)
+    return create_engine(url)
+
+def summarize_df(df):
+    print("Summary of Data\n")
+    print("Rows & Columns:\n")
+    print(df.shape)
+    print("Variables:\n")
+    var_names = df.columns.values
+    print(var_names)
+    print("\nColumn Info:\n")
+    print(df.info())
+    print("\nNumeric Summary Stats:\n")
+    print(df.describe())
+    print("\nTop 5 Values:\n")
+    for var in var_names:
+        print(var+":")
+        print(df[var].value_counts().head())
+        print('\n')
+    print("\nMissing Values:\n")
+    null_counts = df.isnull().sum()
+    if len(null_counts[null_counts > 0]) == 0:
+        print("No missing values")
+    else:
+        print(null_counts[null_counts > 0])
+    print("\nFirst 5 rows:\n")
+    print(df.head())
+    print("\nEnd of Summary")
+
+if __name__ == '__main__':
+    print('Hello, Ada! Are you excited for early release?')
